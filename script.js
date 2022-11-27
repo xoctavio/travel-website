72696219e5f716f3b8c81ac3d5f12d680c84ad08
xoctavio <xavieroctavio91@hotmail.com>
2022-11-27 09:37:39 -0600

    Edited variables and function
    - Removed const nav
    - Removed properties relating to nav in the navToggle function

--- script.js
+++ script.js
@@ -4,12 +4,9 @@ const toggleNav = () => {
 
 // toggle
 const btn = document.getElementById("menu-btn")
-const nav = document.getElementById("menu")
 
 function navToggle(){
  btn.classList.toggle("open")
- nav.classList.toggle("hidden")
- document.body.classList.toggle("no-scroll")
 }
 
 btn.addEventListener("click", navToggle)
